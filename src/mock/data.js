f2b729d761d88f0c74f2467485185b23307670d0
Brennon Nuckols <brennuck@gmail.com>
2020-09-10 09:25:33 -0600

    fix

--- src/mock/data.js
+++ src/mock/data.js
@@ -41,7 +41,6 @@ export const projectsData = [
     img: 'img2txt2.png',
     title: 'IMG 2 TEXT',
     info: 'Img 2 Text is a web application that allows users to convert an image into text',
-    // info2: 'I created this project in a few hours.',
     url: 'https://covid-map-omega.vercel.app/',
     repo: 'https://github.com/brennuck/Covid_Map/tree/master/front-end', // if no repo, the button will not show up
   },
@@ -50,6 +49,7 @@ export const projectsData = [
     img: 'covid-map.png',
     title: 'Covid-19 Tracker',
     info: 'Covid-19 Tracker is a web application that shows Covid statitics for each country.',
+    info2: 'I created this project in a few hours.',
     url: 'https://image-text-converter.vercel.app/',
     repo: 'https://github.com/brennuck/ImageTextConverter/tree/master/image_txt', // if no repo, the button will not show up
   },
